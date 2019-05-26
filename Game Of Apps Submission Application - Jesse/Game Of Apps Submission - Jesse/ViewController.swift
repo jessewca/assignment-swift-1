0b0692e91487ff2dd7d0d2e347722079dd42e1a6
Jesse Wang <wangjesse2002@gmail.com>
2019-05-26 09:54:54 -0700

    Really commit IOS app.

---
+++ Game Of Apps Submission Application - Jesse/Game Of Apps Submission - Jesse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game Of Apps Submission - Jesse
+//
+//  Created by Jesse Wang on 2019-05-24.
+//  Copyright © 2019 Jesse Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
